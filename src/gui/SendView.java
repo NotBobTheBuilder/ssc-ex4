47ee3e859ebe10eeb3f35308ed352d5e085893e7
Jack Wearden <jack@jackwearden.co.uk>
2013-11-15 01:04:37 +0000

    Show message sent dialog

--- src/gui/SendView.java
+++ src/gui/SendView.java
@@ -7,12 +7,14 @@ import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.JOptionPane;
 
 import ex3.mx.SMTP;
 
@@ -48,6 +50,11 @@ public class SendView extends JFrame
     this.setVisible(true);
   }
 
+  public void messageSent() {
+    JOptionPane.showMessageDialog(this, "Message Sent!");
+    this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+  }
+
   @Override
   public void actionPerformed(ActionEvent e) {
       new Runnable() {
@@ -61,6 +68,7 @@ public class SendView extends JFrame
               MSGVIEW.getFile(),
               MSGVIEW.getMessage()
             );
+            messageSent();
           } catch (Exception _) {
             System.out.println("Error Sending Message");
             System.out.println(_);
